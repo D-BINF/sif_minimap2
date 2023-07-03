ac523522849f93fb05d641c31c96608e96992c8b
Derrick Lin <d.lin@garvan.org.au>
2023-07-03 13:44:37 +1000

    added improved exec command with output redirection

--- run_test.sh
+++ run_test.sh
@@ -2,6 +2,9 @@
 
 image="${1:-container.sif}"
 
-singularity exec --bind ./test:/test "$image" minimap2 -a /minimap2/test/MT-human.fa /minimap2/test/MT-orang.fa
+# https://stackoverflow.com/questions/52186041/singularity-exec-echo-redirect-issue
+# using sh -c and "" the entire command for proper redirection executed INSIDE the
+# container.
+singularity exec --bind ./test:/test "$image" sh -c "minimap2 -a /minimap2/test/MT-human.fa /minimap2/test/MT-orang.fa > /test/output.sam"
 
 exit $?
